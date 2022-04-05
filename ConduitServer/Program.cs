da73cec0d12425caebfa89b551af866e1c8728f9
JustLink <linki_mail@mail.ru>
2022-04-05 23:35:46 +0400

    Exp #2

--- ConduitServer/Program.cs
+++ ConduitServer/Program.cs
@@ -19,17 +19,21 @@ static class Program
         var client = listener.AcceptTcpClient();
         Console.WriteLine($"Client {client.Client.LocalEndPoint} connected");
 
+        Thread.Sleep(2000);
+
+        var countBytes = client.Available;
+        Console.WriteLine($"Received {countBytes} bytes");
+
         ReceiveClient(client);
     }
     private static void ReceiveClient(TcpClient client)
     {
         var stream = client.GetStream();
         ReadPacket(stream);
-        
 
         client.Close();
     }
-    
+
     private static void ReadPacket(Stream stream)
     {
         var countInPacket = ReadVarInt(stream);
@@ -38,7 +42,7 @@ static class Program
         var serverAddress = ReadVarString(stream);
         var serverPort = ReadUShort(stream);
         var nextState = ReadVarInt(stream);
-        
+
         Console.WriteLine("Get packet:");
         Console.WriteLine($"[{packetId}](length={countInPacket})");
         Console.WriteLine("HandShake");
@@ -47,17 +51,56 @@ static class Program
         Console.WriteLine("ServerPort=" + serverPort);
         Console.WriteLine("NextState=" + nextState);
 
-        Console.WriteLine();
-
-        if (nextState == 2)
+        if (nextState == 2) // Login
         {
             countInPacket = ReadVarInt(stream);
             packetId = ReadVarInt(stream);
             var username = ReadVarString(stream);
 
+            Console.WriteLine();
             Console.WriteLine("Get packet:");
             Console.WriteLine($"[{packetId}](length={countInPacket})");
             Console.WriteLine("Username=" + username);
+
+            WriteVarInt(stream, 1 + 16 + Encoding.UTF8.GetBytes(username).Length);
+            WriteVarInt(stream, 2);
+            WriteUUID(stream, Guid.NewGuid());
+            WriteVarString(stream, username);
+
+            var buffer = new byte[64];
+            stream.Read(buffer);
+
+            foreach (var data in buffer)
+                Console.WriteLine(data + ", ");
+        }
+        else if (nextState == 1) // Status
+        {
+            countInPacket = ReadVarInt(stream);
+            packetId = ReadVarInt(stream);
+
+            Console.WriteLine();
+            Console.WriteLine("Get packet:");
+            Console.WriteLine($"[{packetId}](length={countInPacket})");
+            Console.WriteLine("Request");
+            
+            var statusText2 = @"{""version"": {""name"": ""Hell server 1.18"",""protocol"": 757},""players"": {""max"": 666,""online"": 99}}";
+            WriteVarInt(stream, 1 + 1 + Encoding.UTF8.GetBytes(statusText2).Length);
+            WriteVarInt(stream, 0);
+            WriteVarString(stream, statusText2);
+
+            countInPacket = ReadVarInt(stream);
+            packetId = ReadVarInt(stream);
+            var pingPayload = ReadLong(stream);
+
+            Console.WriteLine();
+            Console.WriteLine("Get packet:");
+            Console.WriteLine($"[{packetId}](length={countInPacket})");
+            Console.WriteLine("Ping");
+            Console.WriteLine("Payload=" + pingPayload);
+
+            WriteVarInt(stream, 1 + 8);
+            WriteVarInt(stream, 1);
+            WriteLong(stream, pingPayload);
         }
     }
 
@@ -76,7 +119,50 @@ static class Program
 
         return buffer;
     }
-    
+
+    private static void WriteVarString(Stream stream, string data)
+    {
+        var stringData = Encoding.UTF8.GetBytes(data);
+
+        WriteVarInt(stream, stringData.Length);
+        stream.Write(stringData);
+    }
+    private static void WriteVarInt(Stream stream, int number)
+    {
+        const int countBitsInSegment = 7;
+        const int segmentMask = 0x7F;
+        const int continueBitMask = 0x80;
+
+        while ((number & ~segmentMask) != 0)
+        {
+            stream.WriteByte((byte)((number & segmentMask) | continueBitMask));
+            number = (int)((uint)number >> countBitsInSegment);
+        }
+        stream.WriteByte((byte)number);
+    }
+    private static void WriteVarLong(Stream stream, long number)
+    {
+        const int countBitsInSegment = 7;
+        const int segmentMask = 0x7F;
+        const int continueBitMask = 0x80;
+
+        while ((number & ~segmentMask) != 0)
+        {
+            stream.WriteByte((byte)((number & segmentMask) | continueBitMask));
+            number = (long)((ulong)number >> countBitsInSegment);
+        }
+        stream.WriteByte((byte)number);
+    }
+
+    private static void WriteUUID(Stream stream, Guid guid)
+    {
+        stream.Write(guid.ToByteArray());
+    }
+    private static void WriteLong(Stream stream, long number)
+    {
+        stream.Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(number)));
+    }
+
     private static ushort ReadUShort(Stream stream)
     {
         var byte1 = stream.ReadByte();
@@ -94,6 +180,15 @@ static class Program
 
         return BitConverter.ToUInt16(buffer);
     }
+    private static long ReadLong(Stream stream)
+    {
+        var buffer = new byte[8];
+
+        stream.Read(buffer);
+
+        return IPAddress.NetworkToHostOrder(BitConverter.ToInt64(buffer, 0));
+    }
+
     private static string ReadVarString(Stream stream)
     {
         var length = ReadVarInt(stream);
