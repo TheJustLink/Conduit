40f6338f9e4e1e095e21dbdb682661291990c8b6
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2022-04-06 18:24:28 +0300

    Эксперимент #3

--- ConduitServer/Program.cs
+++ ConduitServer/Program.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
+using System.Xml.Linq;
+
+using static System.Net.Mime.MediaTypeNames;
 
 static class Program
 {
@@ -24,14 +28,73 @@ static class Program
         Console.WriteLine("Protocol Version: " + ReadVarInt(stream));
         Console.WriteLine("Server Adress: " + ReadString(stream));
         Console.WriteLine("Server Port: " + ReadPort(stream));
-        Console.WriteLine("Next State: " + ReadVarInt(stream));
+        var nextState = ReadVarInt(stream);
+        Console.WriteLine("Next State: " + nextState);
 
-        Console.WriteLine("[Read Second Packet]");
-        Console.WriteLine("Packet Length: " + ReadVarInt(stream));
-        Console.WriteLine("Packet ID: " + ReadVarInt(stream));
+        if(nextState == 1)
+        {
+            Console.WriteLine("[Read Second Packet]");
+            Console.WriteLine("Packet Length: " + ReadVarInt(stream));
+            Console.WriteLine("Packet ID: " + ReadVarInt(stream));
+            var data = new List<byte>();
+            var result = new List<byte>();
+            WriteVarInt(0, data);
+            WriteString(@"{""version"": {""name"": ""1.18"",""protocol"": 757},""players"": {""max"": 666,""online"": 1200}}", data);
+            WriteVarInt(data.Count, result);
+
+            foreach (var b in data)
+            {
+                result.Add(b);
+            }
+            stream.Write(result.ToArray(), 0, result.Count);
+
+            Console.WriteLine("[Read Third Packet]");
+            Console.WriteLine("Packet Length: " + ReadVarInt(stream));
+            Console.WriteLine("Packet ID: " + ReadVarInt(stream));
+            var value = ReadLong(stream);
+            Console.WriteLine("Long: " + value);
+
+            var pongData = new List<byte>();
+            var pongResult = new List<byte>();
+            WriteVarInt(1, pongData);
+            WriteLong(value, pongData);
+            WriteVarInt(pongData.Count, pongResult);
+
+            foreach (var b in pongData)
+            {
+                pongResult.Add(b);
+            }
+            stream.Write(pongResult.ToArray(), 0, pongResult.Count);
+        }
+        else if(nextState == 2)
+        {
+            Console.WriteLine("[Read Second Packet]");
+            Console.WriteLine("Packet Length: " + ReadVarInt(stream));
+            Console.WriteLine("Packet ID: " + ReadVarInt(stream));
+            var username = ReadString(stream);
+            Console.WriteLine("Username: " + username);
+            var guid = Guid.NewGuid().ToByteArray();
+
+            var resultLogin = new List<byte>();
+            var loginData = new List<byte>();
+            WriteVarInt(2, loginData);
+            foreach(var b in guid)
+            {
+                loginData.Add(b);
+            }
+            WriteString(username, loginData);
+            WriteVarInt(loginData.Count, resultLogin);
+            foreach(var b in loginData)
+            {
+                resultLogin.Add(b);
+            }
+
+            stream.Write(resultLogin.ToArray(), 0, resultLogin.Count);
+        }
+
         //Console.WriteLine("Username: " + ReadString(stream));
-    }
 
+    }
     static int ReadVarInt(Stream stream)
     {
         var value = 0;
@@ -47,11 +110,15 @@ static class Program
         }
         return value | ((b & 0x7F) << (size * 7));
     }
+    static long ReadLong(Stream stream)
+    {
+        var l = Read(8, stream);
+        return IPAddress.NetworkToHostOrder(BitConverter.ToInt64(l, 0));
+    }
     static string ReadString(Stream stream)
     {
         var length = ReadVarInt(stream);
         var stringValue = Read(length, stream);
-
         return Encoding.UTF8.GetString(stringValue);
     }
     static ushort ReadPort(Stream stream)
@@ -71,9 +138,8 @@ static class Program
         return buffered.ToArray();
     }
 
-    static void WriteVarInt(int integer, out List<byte> buffer)
+    static void WriteVarInt(int integer, List<byte> buffer)
     {
-        buffer = new List<byte>();
         while ((integer & -128) != 0)
         {
             buffer.Add((byte)(integer & 127 | 128));
@@ -96,14 +162,24 @@ static class Program
         }
         return value | ((b & 0x7F) << (size * 7));
     }
-    static void WriteString(string str, out List<byte> buffer)
+    static void WriteString(string str, List<byte> buffer)
     {
-        buffer = new List<byte>();
         byte[] data = Encoding.UTF8.GetBytes(str);
-        WriteVarInt(data.Length, out buffer);
-        foreach(var b in data)
+        WriteVarInt(data.Length, buffer);
+        foreach (var b in data)
         {
             buffer.Add(b);
+        }
+    }
+    static void WriteLong(long data, List<byte> buffer)
+    {
+        Write(BitConverter.GetBytes(IPAddress.HostToNetworkOrder(data)), buffer);
+    }
+    static void Write(byte[] data, List<byte> buffer)
+    {
+        foreach (var i in data)
+        {
+            buffer.Add(i);
         }
     }
 }
