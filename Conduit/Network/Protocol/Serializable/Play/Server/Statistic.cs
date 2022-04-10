3bececfc2ecd88898ef68b21145dcff7aaeb2b4a
TailsTek <60035122+TailsTek@users.noreply.github.com>
2022-04-10 23:38:49 +0900

    вф

---
+++ Conduit/Network/Protocol/Serializable/Play/Server/Statistic.cs
@@ -0,0 +1,19 @@
+﻿using Conduit.Network.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conduit.Network.Protocol.Serializable.Play.Server
+{
+    public sealed class Statistic
+    {
+        [VarInt]
+        public int CategoryID;
+        [VarInt]
+        public int StatisticID;
+        [VarInt]
+        public int Value;
+    }
+}
